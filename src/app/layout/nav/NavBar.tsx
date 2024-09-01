d68a315da8643bf571c30398ee31e84ea0b2f891
Rasgitrepo <diaw@ras.ac.th>
2024-09-01 11:27:35 +0700

    End of Section 5

--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -1,31 +1,32 @@
+import { NavLink } from "react-router-dom";
 import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
+import SignedInMenu from "./SignedInMenu";
+import { useState } from "react";
+import SignedOutButton from "./SignedOutButton";
 
-type Props = {
-    setFormOpen: (value: boolean) => void;
-}
 
-export default function NavBar({setFormOpen}: Props) {
-  return (
-    <Menu inverted={true} fixed="top">
-        <Container>
-            <MenuItem header>
-                <img src="/logo.png" alt="logo" />
-                Re-vents
-            </MenuItem>
-            <MenuItem name="Events" />
-            <MenuItem>
-                <Button 
-                onClick={()=>setFormOpen(true)}
-                floated="right" 
-                positive={true} 
-                inverted={true} 
-                content='Create event' />
-            </MenuItem>
-            <MenuItem position="right">
-                <Button basic inverted content='Login' />
-                <Button basic inverted content='Register' style={{marginLeft: '0.5em'}} />
-            </MenuItem>
-        </Container>
-    </Menu>
-  )
+export default function NavBar() {
+    const [auth, setAuth] = useState(true);
+
+    return (
+        <Menu inverted={true} fixed="top">
+            <Container>
+                <MenuItem header as={NavLink} to='/'>
+                    <img src="/logo.png" alt="logo" />
+                    Re-vents
+                </MenuItem>
+                <MenuItem name="Events" as={NavLink} to='/events' />
+                <MenuItem>
+                    <Button
+                        as={NavLink}
+                        to='/createEvent'
+                        floated="right"
+                        positive={true}
+                        inverted={true}
+                        content='Create event' />
+                </MenuItem>
+                {auth ? <SignedInMenu setAuth={setAuth} /> : <SignedOutButton setAuth={setAuth} />}
+            </Container>
+        </Menu>
+    )
 }
---
+++ src/app/router/Routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from '../layout/App';
+import EventDashboard  from "../../features/events/dashboard/EventDashboard";
+import EventForm from "../../features/events/form/EventForm";
+import EventDetailedPage from "../../features/events/details/EventDetailedPage";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {path: '/events', element: <EventDashboard />},
+            {path: '/events/:id', element: <EventDetailedPage />},
+            {path: '/manage/:id', element: <EventForm />},
+            {path: '/createEvent', element: <EventForm />},
+        ]
+    }
+]) 
