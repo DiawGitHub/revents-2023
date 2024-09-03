9a81acb799dafc65f268bd98a19cf195d52f1dfc
Rasgitrepo <diaw@ras.ac.th>
2024-09-03 18:22:21 +0700

    End of Section 6

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -1,16 +1,17 @@
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
-import { sampleData } from "../../../app/api/sampleData";
-import { useEffect, useState } from "react";
-import { AppEvent } from "../../../app/types/event";
+//import { sampleData } from "../../../app/api/sampleData";
+//import { useEffect, useState } from "react";
+//import { AppEvent } from "../../../app/types/event";
+import { useAppSelector } from "../../../app/store/store";
 
 export default function EventDashboard() {
-  const [events, setEvents] = useState<AppEvent[]>([]);
+  // const [events, setEvents] = useState<AppEvent[]>([]);
 
-  useEffect(() => {
-    setEvents(sampleData);
-  }, [])
-
+  // useEffect(() => {
+  //   setEvents(sampleData);
+  // }, [])
+  const {events} = useAppSelector(state => state.events)
 
   return (
     <Grid>
